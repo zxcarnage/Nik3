88bc82dc98384033931573ce426fb34c3dc49e0b
zxcarnage <nmoysenovich@gmail.com>
2023-12-03 16:47:47 +0300

    finished

---
+++ Assets/Scripts/Card/Card.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class Card : MonoBehaviour,IPointerClickHandler
+{
+    [SerializeField] private CardInfo _cardInfo;
+
+    private Image _image;
+
+    public event Action<Card> CardOpened;
+    public CardInfo CardInfo => _cardInfo;
+
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    private void OpenCard()
+    {
+        _image.sprite = _cardInfo.Picture;
+        CardOpened?.Invoke(this);
+    }
+    
+
+    public void CloseCard()
+    {
+        _image.sprite = _cardInfo.Back;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        OpenCard();
+    }
+}
